6fbaee2f44843469e82edc96ca76f5074fc9f98e
pablolopezgil1990 <pablolopezgil1990@gmail.com>
2026-01-08 00:17:23 +0100

    Update App.tsx
    boton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-    import React, { useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import { AnimatePresence, motion, useReducedMotion } from "framer-motion";
 import {
   Mail,
